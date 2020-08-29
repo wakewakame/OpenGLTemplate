a1d3dedbf61558d57e7496d2cf562fb2c470e5a1
wakewakame <0214.taiki@gmail.com>
2020-08-30 06:30:59 +0900

    init

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+// Include standard headers
+#include <iostream>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+
+// Include GLM
+#include <glm/glm.hpp>
+
+// Include OpenAL
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <AL/alext.h>
+#include <common/alhelpers.h>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
